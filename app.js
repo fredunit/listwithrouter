fca5277ef15ab28e586426882e0ccb5a0204e873
ho lun chow <fredunit2007@gmail.com>
2021-05-05 21:22:30 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json').results
 const Restaurant = require('./models/restaurant')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -23,14 +24,16 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
-//setting static files
+
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 //routes setting
 app.get('/', (req, res) => {
   Restaurant.find()
   .lean()
+  .sort({ _id: 'asc'})
   .then(restaurant => res.render('index', { restaurant }))
   .catch(error => console.log(error))
 })
